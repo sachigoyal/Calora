f1dfe87368fb16f796b5bb06811bd038faa5a5dc
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-12 23:55:16 +0530

    priv layout

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import { ReactNode } from "react";
+
+
+export default function PrivateLayout({children} : {children: ReactNode}){
+
+    return (
+        <>
+        <Navbar/>
+        <main>{children}</main>
+        </>
+    )
+}
