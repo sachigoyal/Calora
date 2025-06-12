a5bfc94c470e12eb38a99fae76beda22a2584c8f
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-12 23:57:39 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { Button } from "@/components/ui/button";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
+import Navbar from "@/components/Navbar";
 
 export default async function Home() {
   const { userId } = await auth();
   if (userId != null) redirect("/events");
   return (
+  
     <div className="text-center container mx-auto my-4">
+        <Navbar/>
       <div className="text-3xl mb-4">Home Page</div>
       <div className="gap-2 flex justify-center">
 
---
+++ src/components/CopyEventButton.tsx
@@ -0,0 +1,5 @@
+
+
+export function CopyEventButton () {
+    return <div>hi</div>
+}
