44a309ef7ce7bcc478ea7a019e4ec1798fc2b5c7
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-13 23:52:11 +0530

    event action

--- src/server/actions/event.ts
+++ src/server/actions/event.ts
@@ -5,6 +5,8 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/drizzle/db";
 import { redirect } from "next/navigation";
 import { EventTable } from "@/drizzle/schema";
+import { and, eq } from "drizzle-orm";
+import { tr } from "zod/v4/locales";
 
 export async function createEvent(
   unsafeData: z.infer<typeof eventFormSchema>
@@ -17,5 +19,42 @@ export async function createEvent(
   }
 
   await db.insert(EventTable).values({ ...data, clerkUserId: userId });
+  redirect("/events");
+}
+
+export async function updateEvent(
+  id: string,
+  unsafeData: z.infer<typeof eventFormSchema>
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = await auth();
+  const { success, data } = eventFormSchema.safeParse(unsafeData);
+
+  if (!success || userId == null) {
+    return { error: true };
+  }
+
+ const {rowCount} =  await db.update(EventTable).set({...data}).where(and(eq(EventTable.id, id), eq(EventTable.clerkUserId, userId)))
+
+ if(rowCount === 0) {
+  return {error: true}
+ }
+  redirect("/events");
+}
+
+
+export async function deleteEvent(
+  id: string,
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = await auth();
+
+  if (userId == null) {
+    return { error: true };
+  }
+
+ const {rowCount} =  await db.delete(EventTable).where(and(eq(EventTable.id, id), eq(EventTable.clerkUserId, userId)))
+
+ if(rowCount === 0) {
+  return {error: true}
+ }
   redirect("/events");
 }
