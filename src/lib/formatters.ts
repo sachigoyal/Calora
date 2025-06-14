592380e6075d3fc3dc71f5dc36bc94eaa298e457
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-14 19:33:40 +0530

    update

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,12 +1,19 @@
+export function formatEventTime(durationInMinutes: number) {
+  const hours = Math.floor(durationInMinutes / 60);
+  const minutes = durationInMinutes % 60;
+  const minutesString = `${minutes} ${minutes > 1 ? "mins" : "min"}`;
+  const hoursString = `${hours} ${hours > 1 ? "hrs" : "hr"}`;
 
+  if (hours === 0) return minutesString;
+  if (minutes === 0) return hoursString;
+  return `${hoursString} ${minutesString}`;
+}
 
-export function formatEventTime (durationInMinutes: number) {
- const hours = Math.floor(durationInMinutes / 60);
- const minutes = durationInMinutes % 60;
- const minutesString = `${minutes} ${minutes > 1 ? "mins" : "min"}`;
-   const hoursString = `${hours} ${hours > 1 ? "hrs" : "hr"}`
-
-     if (hours === 0) return minutesString
-  if (minutes === 0) return hoursString
-  return `${hoursString} ${minutesString}`
+export function formatTimezoneOffset(timezone: string) {
+  return new Intl.DateTimeFormat(undefined, {
+    timeZone: timezone,
+    timeZoneName: "shortOffset",
+  })
+    .formatToParts(new Date())
+    .find((part) => part.type == "timeZoneName")?.value;
 }
