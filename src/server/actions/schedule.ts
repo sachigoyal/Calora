0cd68d0752d0892317bdaa47d7c6d0efe29969b4
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-14 19:33:31 +0530

    schedule action

--- src/server/actions/schedule.ts
+++ src/server/actions/schedule.ts
@@ -1,45 +1,50 @@
+"use server";
 import { db } from "@/drizzle/db";
 import { ScheduleAvailabilitiesTable, ScheduleTable } from "@/drizzle/schema";
 import { scheduleFormSchema } from "@/schema/schedule";
 import { auth } from "@clerk/nextjs/server";
-import { error } from "console";
 import { eq } from "drizzle-orm";
 import { BatchItem } from "drizzle-orm/batch";
 import { z } from "zod";
 
+export async function saveSchedule(
+  unsafeData: z.infer<typeof scheduleFormSchema>
+) {
+  const { userId } = await auth();
+  const { success, data } = scheduleFormSchema.safeParse(unsafeData);
 
-export async function saveSchedule(unsafeData: z.infer<typeof scheduleFormSchema>){
-   
-    const {userId} = await auth()
-    const {success, data} = scheduleFormSchema.safeParse(unsafeData)
+  if (!success || userId == null) {
+    return { error: true };
+  }
 
-    if(!success || userId == null){
-        return {error: true}
-    }
+  const { availabilities, ...scheduledData } = data;
 
-    const {availabilities, ...scheduledData} = data
-
-    const [{id: scheduleId}] = await db.insert(ScheduleTable).values({...scheduledData, clerkUserId: userId}).onConflictDoUpdate({
-        target: ScheduleTable.clerkUserId,
-        set: scheduledData,
+  const [{ id: scheduleId }] = await db
+    .insert(ScheduleTable)
+    .values({ ...scheduledData, clerkUserId: userId })
+    .onConflictDoUpdate({
+      target: ScheduleTable.clerkUserId,
+      set: scheduledData,
     })
-    .returning({id: ScheduleTable.id})
+    .returning({ id: ScheduleTable.id });
 
-    const statements: [BatchItem<"pg">]= [
-        db.delete(ScheduleAvailabilitiesTable).where(eq(ScheduleAvailabilitiesTable.scheduleId, scheduleId))
-    ]
+  const statements: [BatchItem<"pg">] = [
+    db
+      .delete(ScheduleAvailabilitiesTable)
+      .where(eq(ScheduleAvailabilitiesTable.scheduleId, scheduleId)),
+  ];
 
-    if(availabilities.length > 0){
-         statements.push(
+  if (availabilities.length > 0) {
+    statements.push(
       db.insert(ScheduleAvailabilitiesTable).values(
-        availabilities.map(availability => ({
+        availabilities.map((availability) => ({
           ...availability,
           dayOfweek: availability.dayOfWeek,
           scheduleId,
         }))
       )
-    )
-    }
+    );
+  }
 
-      await db.batch(statements)
+  await db.batch(statements);
 }
