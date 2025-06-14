8b6e34d0ee8053f82c3a1ab89b311e31f508da7d
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-14 19:33:17 +0530

    schedule page

--- src/app/(private)/schedule/page.tsx
+++ src/app/(private)/schedule/page.tsx
@@ -1,7 +1,24 @@
-export default function SchedulePage() {
+
+import ScheduleForm from "@/components/forms/ScheduleForm";
+import { Card, CardContent } from "@/components/ui/card";
+import { auth } from "@clerk/nextjs/server"
+import { db } from "@/drizzle/db"
+
+export default async function NewEventPage() {
+  const { userId, redirectToSignIn } = await auth()
+  if (userId == null) return redirectToSignIn()
+
+      const schedule = await db.query.ScheduleTable.findFirst({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    with: { availabilities: true },
+  })
   return (
-    <div>
-      <h1>Schedule</h1>
+    <div className="w-full max-w-xl mx-auto my-8">
+      <Card>
+        <CardContent>
+         <ScheduleForm schedule={schedule}/>
+        </CardContent>
+      </Card>
     </div>
   );
 }
