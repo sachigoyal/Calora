9acb6071a9884fcedc4ec34ce13b827a638edf3d
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-17 23:49:48 +0530

    meeting schema

---
+++ src/schema/meetings.ts
@@ -0,0 +1,19 @@
+import { startOfDay } from "date-fns";
+import { z } from "zod";
+
+const meetingSchemaBase = z.object({
+  startTime: z.date().min(new Date()),
+  guestName: z.string().min(1, "Required"),
+  guestEmail: z.string().email().min(1, "Required"),
+  guestNotes: z.string().optional(),
+  timezone: z.string().min(1, "Required"),
+});
+
+export const meetFormSchema = z.object({
+  date: z.date().min(startOfDay(new Date()), "Must be in the future"),
+}).merge(meetingSchemaBase)
+
+export const meetingActionSchema = z.object({
+    eventId: z.string().min(1, "Required"),
+    clerkUserId: z.string().min(1, "Required"),
+}).merge(meetingSchemaBase)
